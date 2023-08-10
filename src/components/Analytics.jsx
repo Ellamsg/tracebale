3a71f40115381888065dedaab3229d35ab0e109f
Ellams George <ellamsgeorge7@gmail.com>
2023-08-11 00:34:07 +0100

    Demo page

--- src/components/Analytics.jsx
+++ src/components/Analytics.jsx
@@ -2,26 +2,39 @@ import React from "react";
 
 export default function Analytics() {
   return (
-    <div className="">
-      <div className="flex">
-        <div className="bg-red">
-          <img src="lady2.png" />
+    <div className="bg-[#0F265C]">
+      <div className="flex lg:flex-row flex-col gap-5">
+        <div className="">
+          <img className="" src="lady2.png" />
         </div>
 
-
-
-        <div className="flex justify-center items-end gap-3">
-
-
-            <div className="bg-green histogram h-[200px]">
-             farmers
+        <div className="flex   justify-center items-end gap-4 px-2 lg:px-0 lg:gap-7">
+          <div>
+            <p className="histogram-text">9Lacs</p>
+            <div className="bg-[#DDF6E6] h-[297px] histogram ">
+              <p className="histogram-text2">farmers registered</p>
             </div>
-            <div className="bg-yellow histogram h-[100px]">
-                man
-                </div>
-                <div className="bg-blue histogram">
-                goat
-                </div>
+          </div>
+
+          <div>
+            <p className="histogram-text">9Lacs</p>
+            <div className="bg-[#FFCCD3] histogram h-[435px]">
+              <p className="histogram-text2">farmers registered</p>
+            </div>
+          </div>
+          <div>
+            <p className="histogram-text">9Lacs</p>
+            <div className="bg-[#88CDA1] histogram h-[165px]">
+              <p className="histogram-text2">farmers registered</p>
+            </div>
+          </div>
+
+          <div>
+            <p className="histogram-text">9Lacs</p>
+            <div className="bg-[#C7B9F6] histogram h-[353px]">
+              <p className="histogram-text2">farmers registered</p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,11 +2,13 @@ import React, { useContext } from "react";
 import Landing from "../components/Landing";
 import Knowmore from "../components/Knowmore";
 import Analytics from "../components/Analytics";
+import Demo from "../components/Demo";
 export default function Home() {
   return (
     <div className="">
      <Landing/>
      <Analytics/>
+     <Demo/>
      <Knowmore/>
     </div>
   );
