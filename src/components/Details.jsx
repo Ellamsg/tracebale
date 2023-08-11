d4d58320d3dbd42f544b009be15b0093b563fe43
Ellams George <ellamsgeorge7@gmail.com>
2023-08-11 12:22:37 +0100

    BTN

---
+++ src/components/Details.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+
+export default function Details() {
+  return (
+    <div className="">
+      <div className="">
+        <div>
+          <p>Tracable-one platform to make ur work Go round</p>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nulla non,
+            commodi minus at illo similique, unde odio iusto quidem error .
+          </p>
+        </div>
+
+        <div className="flex items-center ">
+          <div>
+            <img src="world.png" alt />
+         
+          </div>
+
+          <div className="flex flex-col gap-4">
+            <div className="isolation ">
+              <img src="Isolation1.png" />
+              <p>lllglhhh;hhlhlkhn gjhghbgjgjjb gjk</p>
+            </div>
+            <div className="isolation ">
+              <img src="Isolation1.png" />
+              <p>lllglhhh;hhlhlkhn gjhghbgjgjjb gjk</p>
+            </div>
+            <div className="isolation ">
+              <img src="Isolation1.png" />
+              <p>lllglhhh;hhlhlkhn gjhghbgjgjjb gjk</p>
+            </div>
+            <div className="isolation ">
+              <img src="Isolation1.png" />
+              <p>lllglhhh;hhlhlkhn gjhghbgjgjjb gjk</p>
+            </div>
+            <button className="btn">KNOW PLATFORM IN DETAILS</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,12 +3,14 @@ import Landing from "../components/Landing";
 import Knowmore from "../components/Knowmore";
 import Analytics from "../components/Analytics";
 import Demo from "../components/Demo";
+import Details from "../components/Details";
 export default function Home() {
   return (
     <div className="">
      <Landing/>
      <Analytics/>
      <Demo/>
+     <Details/>
      <Knowmore/>
     </div>
   );
