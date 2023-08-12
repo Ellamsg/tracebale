3787a7e7b2921e85bb153e7694aaf204e7aace87
Ellams George <ellamsgeorge7@gmail.com>
2023-08-12 21:35:13 +0100

    format

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,50 +1,33 @@
-
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { AiOutlineAlignRight } from "react-icons/ai";
 export default function Nav() {
-
-  
   let Links = [
     { name: "PLATFORM", link: "/" },
     { name: "ADVANTAGES", link: "/" },
     { name: "NEWS & RESOURCES    v", link: "/" },
     { name: "REQUESt FOR DEMO", link: "/" },
-   
-   
-    
-   
   ];
   let [open, setOpen] = useState(false);
   return (
     <div className=" bg-oceanblue bg-[#CBDAFB] z-50 sticky lg:py-0   top-0 ">
-
       <div className=" sticky md:px-1 lg:px-6  top-0 left-0 md:p-0  lg:text-xl  text-[16px]">
         <div className="md:flex items-center py-3 lg:py-4  md:py-2 justify-between lg:gap-6  ">
           <div className="md:pl-0 pl-2 ml-2 lg:ml-0   ">
-          <img src="tracebale2.png" className=""/>
+            <img src="tracebale2.png" className="" />
           </div>
 
           <div
             onClick={() => setOpen(!open)}
             className=" absolute  right-2 top-[28px] z-10 cursor-pointer md:hidden"
           >
-            <div className="w-4  mr-2  "
-              name={open ? "close" : "menu"}
-             
-            >
-
+            <div className="w-4  mr-2  " name={open ? "close" : "menu"}>
               <div className="dsh">
-                <AiOutlineAlignRight className="text-[30px]"/>
+                <AiOutlineAlignRight className="text-[30px]" />
               </div>
-              <div className="dsh">
-                                  
-                </div>
-                <div className="dsh">
-                                  
-                </div>
+              <div className="dsh"></div>
+              <div className="dsh"></div>
             </div>
-            
           </div>
 
           <ul
@@ -62,23 +45,19 @@ export default function Nav() {
                       {link.name}
                     </Link>
                   </li>
-                 
                 </div>
-                
               ))}
             </div>
             <Link to="Cart">
-              <div className="  pl-3 justify-center md:py-0 py-[5px]
-               border-boxwind border-2 md:border-0  flex">
-              
-              <div className=" btn pt-[6px] ">
-             <button>LOGIN</button>
+              <div
+                className="  pl-3 justify-center md:py-0 py-[5px]
+               border-boxwind border-2 md:border-0  flex"
+              >
+                <div className=" btn pt-[6px] ">
+                  <button>LOGIN</button>
+                </div>
               </div>
-              </div>
-              
-           
-              </Link>
-           
+            </Link>
           </ul>
         </div>
       </div>
